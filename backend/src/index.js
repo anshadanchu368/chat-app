208f728c54dabbb0fd40730e727df19eb09ae3e8
Hashim Anhad <anshadanchu368@gmail.com>
2025-05-25 22:13:15 +0530

    chat contianer is done alongwith user sidebar

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import messageRoutes from "./routes/message.route.js"
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
+import cors from "cors"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
 const app = express();
@@ -11,8 +13,15 @@ const PORT = process.env.PORT ;
 
 app.use(express.json());
 app.use(cookieParser())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages",messageRoutes );
 
 app.listen(5000, () => {
   console.log("server is running on port "+ PORT);
