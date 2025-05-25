2d98e9fe092f74ad7466c495f289c7a9ca9f4470
Hashim Anhad <anshadanchu368@gmail.com>
2025-05-25 19:02:59 +0530

    chat for store is created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,10 +7,11 @@ import LoginPage from './pages/login'
 import ProfilePages from './pages/Profilepages'
 import { useAuthStore } from './store/useAuthStore'
 import HomePage from './components/HomePage'
+import { useThemeStore } from './store/useThemeStore'
 
 const App = () => {
   const {authUser, checkAuth, isLoading} = useAuthStore()
-
+ const {theme}= useThemeStore()
   useEffect(()=>{
     checkAuth()
   },[checkAuth])
@@ -22,7 +23,7 @@ const App = () => {
  )
 
   return (
-    <div >
+    <div data-theme={theme}>
    <Navbar/>
 
    <Routes>
